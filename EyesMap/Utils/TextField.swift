a78942c79dff309dfca3f1e44a82debb3278ffa0
pbit <alstjqrhkr2018@gmail.com>
2023-08-21 14:32:12 +0900

    [Feat] TextField extension (텍스트 필드 왼쪽 공백)

---
+++ EyesMap/Utils/TextField.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+extension UITextField {
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+}
