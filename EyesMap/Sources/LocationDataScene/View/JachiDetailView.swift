97d4ed466dbcc1b39ac23ef67db591562beb71cc
pbit <alstjqrhkr2018@gmail.com>
2023-08-25 00:25:26 +0900

    [FEAT] 메달 추가 png 추가

---
+++ EyesMap/Sources/LocationDataScene/View/JachiDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  JachiDetailView.swift
+//  EyesMap
+//
+//  Created by 곽민섭 on 2023/08/24.
+//
+
+import UIKit
+
+class JachiDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
